fa9395e7fff09704e4048be3d50cefdd401bb089
Manoj P R <manojpramesh@gmail.com>
2017-12-09 23:45:59 +0530

    writeContract and trycatch

--- helpers/contract.helper.js
+++ helpers/contract.helper.js
@@ -11,7 +11,7 @@ web3.setProvider(new web3.providers.HttpProvider(Config.rpc));
 
 module.exports = {
 
-    compileContract: () => {
+    compileContract: (contract, name) => {
         let compiledContract = solc.compile(contract, 1);
         let abi = compiledContract.contracts[name].interface;
         let contractObj = web3.eth.contract(JSON.parse(abi));
@@ -56,37 +56,60 @@ module.exports = {
 
     readContract: (params, contractAddress, abi, funcName) => {
         return new Promise(function(resolve, reject) {
-            let contract = web3.eth.contract(abi).at(contractAddress);
-            resolve(contract[funcName].call.apply(this, params));
+            try {
+                let contract = web3.eth.contract(abi).at(contractAddress);
+                resolve(contract[funcName].call.apply(this, params));
+            } catch (error) {
+                reject(error);
+            }
         });
     },
 
     readContractArray: (count, contractAddress, abi, funcName) => {
-        return new Promise(function(resolve, reject) {
-            let contract = web3.eth.contract(abi).at(contractAddress);
-            let response = [];
-            for (let i = 0; i < req.params.length; i++) {
-                response.push(contract[req.params.function](req.params.id, i));
+        return new Promise((resolve, reject) => {
+            try {
+                let contract = web3.eth.contract(abi).at(contractAddress);
+                let response = [];
+                for (let i = 0; i < req.params.length; i++) {
+                    response.push(contract[req.params.function](req.params.id, i));
+                }
+                resolve(response);
+            } catch (error) {
+                reject(error);
             }
-            resolve(response);
+
+        });
+    },
+
+    writeContract: (from, contractAddress, abi, value, funcName, params) => {
+        return new Promise((resolve, reject) => {
+            let contract = web3.eth.contract(abi).at(contractAddress);
+            resolve(contract[funcName](...params, {
+                from: from,
+                value: value || 0,
+                gas: Config.gas
+            }));
         });
     },
 
     writeContractRaw: (from, privateKey, value, contractAddress, abi, funcName, params) => {
-        return new Promise(function(resolve, reject) {
-            let tx = new Tx({
-                nonce: web3.toHex(web3.eth.getTransactionCount(from)),
-                gasPrice: web3.toHex(web3.eth.gasPrice),
-                gasLimit: web3.toHex(Config.gas),
-                to: contractAddress,
-                from: from,
-                value: web3.toHex(value),
-                data: new SolidityFunction('', _.find(abi, { name: funcName }), '').toPayload(params).data
-            });
-            tx.sign(new Buffer(privateKey, 'hex'));
-            let serializedTx = tx.serialize();
-            let hash = web3.eth.sendRawTransaction("0x" + serializedTx.toString('hex'));
-            resolve(hash);
+        return new Promise((resolve, reject) => {
+            try {
+                let tx = new Tx({
+                    nonce: web3.toHex(web3.eth.getTransactionCount(from)),
+                    gasPrice: web3.toHex(web3.eth.gasPrice),
+                    gasLimit: web3.toHex(Config.gas),
+                    to: contractAddress,
+                    from: from,
+                    value: web3.toHex(value),
+                    data: new SolidityFunction('', _.find(abi, { name: funcName }), '').toPayload(params).data
+                });
+                tx.sign(new Buffer(privateKey, 'hex'));
+                let serializedTx = tx.serialize();
+                resolve(web3.eth.sendRawTransaction("0x" + serializedTx.toString('hex')));
+            } catch (error) {
+                reject(error);
+            }
         });
     },
 
